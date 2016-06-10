1232f25af1124826495067876ab3b3bb7f53c708
drthomas21 <david.thomas214@yahoo.com>
2016-06-10 10:43:32 -0700

    finishing up framework, fixing bug with angular

--- webserver/public_html/ajax.php
+++ webserver/public_html/ajax.php
@@ -1,16 +1,28 @@
 <?php
 header("Content-Type: application/json");
-if($_SERVER['REQUEST_METHOD'] == "GET" && array_key_exists("stock",$_GET) && !empty($_GET['stock'])) {
+if($_SERVER['REQUEST_METHOD'] == "GET") {
 	require "bootloader.php";
 	$ret = new stdClass();
-	$stock = preg_replace("/[^A-Za-z0-9]/","",$_GET['stock']);
+	$from = $_GET['from'] ? intval($_GET['from']) : 0;
+	$size = $_GET['size'] ? intval($_GET['size']) : 10;
 	
-	$ret->stocks = ElasticSearchDriver::getInstance()->search("stocks",ElasticSearchDriver::SEARCH_TERM,array("stock"=>strtolower($stock)))->getRecords();
-	$amazon = ElasticSearchDriver::getInstance()->search("amazon",ElasticSearchDriver::SEARCH_TERM,array("ticker"=>strtolower($stock)))->getNextRecord();
-	if($amazon) {
-		$name = trim(preg_replace("/(inc\.?)/i","",$amazon->name));
-		$ret->tweets = ElasticSearchDriver::getInstance()->search("tweets-5.0",ElasticSearchDriver::SEARCH_MATCH,array("text"=>strtolower("{$name}")));
+	$ret = new stdClass();
+	
+	if(array_key_exists("stock",$_GET) && !empty($_GET['stock'])) {
+		$stock = preg_replace("/[^A-Za-z0-9]/","",$_GET['stock']);
+		
+		$ret->stocks = ElasticSearchDriver::getInstance()->search("stocks",ElasticSearchDriver::SEARCH_TERM,array("stock"=>strtolower($stock)),$size,$from)->getRecords();
+		if(!isset($_GET['from'])) {
+			$amazon = ElasticSearchDriver::getInstance()->search("amazon",ElasticSearchDriver::SEARCH_TERM,array("ticker"=>strtolower($stock)))->getNextRecord();
+			if($amazon) {
+				$name = trim(preg_replace("/(inc\.?)/i","",$amazon->name));
+				$ret->tweets = ElasticSearchDriver::getInstance()->search("tweets-5.0",ElasticSearchDriver::SEARCH_MATCH,array("text"=>strtolower("{$name}")))->getRecords();
+			}
+		}		
+	} elseif(array_key_exists("tweet",$_GET) && !empty($_GET['tweet'])) {
+		$ret->tweets = ElasticSearchDriver::getInstance()->search("tweets-5.0",ElasticSearchDriver::SEARCH_MATCH,array("text"=>strtolower("{$name}")),$size,$from)->getRecords();
 	}
+	
 	echo json_encode($ret);
 } else {
 	http_status_code(400);
--- webserver/public_html/index.php
+++ webserver/public_html/index.php
@@ -15,7 +15,7 @@
 					<a href="/" class="navbar-brand">Black And Yellow</a>
 					<form class="navbar-form navbar-left" role="search">
 						<div class="form-group">
-							<input type="text" class="form-control" placeholder="AAPL" ng-change="searchData" ng-model="query.value" ng-disabled="!query.enabled"/>
+							<input type="text" class="form-control" placeholder="AAPL" ng-change="searchData()" ng-model="query.value" ng-disabled="!query.enabled"/>
 						</div>
 					</form>
 				</div>
@@ -28,7 +28,7 @@
 		
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-sanitize.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
@@ -36,9 +36,19 @@
 		(function(angular,jQuery){
 			angular.module("datascience",["ngSanitize"])
 			.service("SearchService",["$http",function($http){
-				this.get = function(stock,callback) {
-					$http.get("/ajax.php?query="+stock)
-					.done(function(){
+				this.get = function(query,callback) {
+					var _query = "";
+					
+					for(param in query){
+						_query = param+"="+query[param]+"&";
+					}
+					
+					_query = _query.substr(0,_query.length-1);
+					$http.get("/ajax.php?"+_query)
+					.then(function(response){
+						callback(response.data);
+					},function(response){
+						console.log(response);
 					});
 				};
 			}])
@@ -53,7 +63,11 @@
 						$timeout.cancel(promise);
 					}
 					promise = $timeout(function(){
-						SearchService.get($scope.query.value,function(data){
+						var query = {
+							"stock": $scope.query.value
+						}
+						console.log(SearchService);
+						SearchService.get(query,function(data){
 							$rootScope.$broadcast("event",data);
 						});
 					},500);
