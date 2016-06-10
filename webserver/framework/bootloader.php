4a9fe6d74bf842bf75c1d3f4865fd5acabc0fdcf
drthomas21 <david.thomas214@yahoo.com>
2016-06-10 01:12:21 -0700

    adding elasticsearchresult and memcachedriver

--- webserver/framework/bootloader.php
+++ webserver/framework/bootloader.php
@@ -1,4 +1,4 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    include _DIR__ . "/classes/{$class_name}.inc";
+    include __DIR__ . "/classes/{$class_name}.inc";
 });
--- webserver/public_html/ajax.php
+++ webserver/public_html/ajax.php
@@ -1,10 +1,16 @@
 <?php
 header("Content-Type: application/json");
 if($_SERVER['REQUEST_METHOD'] == "GET" && array_key_exists("stock",$_GET) && !empty($_GET['stock'])) {
+	require "bootloader.php";
 	$ret = new stdClass();
-	$stock = preg_replace("/[^A-Za-z0-9]/",$_GET['stock']);
-	$ret->stocks = ElasticSearchDriver::getInstance()->search("stock",array("stock"=>$stock));
-	$ret->tweets = ElasticSearchDriver::getInstance()->search("tweets",array("text"=>"*{$stock}*"));
+	$stock = preg_replace("/[^A-Za-z0-9]/","",$_GET['stock']);
+	
+	$ret->stocks = ElasticSearchDriver::getInstance()->search("stocks",ElasticSearchDriver::SEARCH_TERM,array("stock"=>strtolower($stock)))->getRecords();
+	$amazon = ElasticSearchDriver::getInstance()->search("amazon",ElasticSearchDriver::SEARCH_TERM,array("ticker"=>strtolower($stock)))->getNextRecord();
+	if($amazon) {
+		$name = trim(preg_replace("/(inc\.?)/i","",$amazon->name));
+		$ret->tweets = ElasticSearchDriver::getInstance()->search("tweets-5.0",ElasticSearchDriver::SEARCH_MATCH,array("text"=>strtolower("{$name}")));
+	}
 	echo json_encode($ret);
 } else {
 	http_status_code(400);
